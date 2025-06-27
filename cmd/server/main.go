791b933b23876432e4bb06594b8f9fa9f8c26a7c
Bryan Redd <bryan.d.redd@gmail.com>
2025-06-27 17:31:41 -0600

    feat: default redirect

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,24 +3,43 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/url"
+	"os"
 
 	"github.com/49pctber/redirect"
 )
 
+var default_redirect string
+
+func RedirectDefault(w http.ResponseWriter, r *http.Request) {
+	if default_redirect != "" {
+		http.Redirect(w, r, default_redirect, http.StatusTemporaryRedirect)
+	} else {
+		fmt.Fprintf(w, "There was an error with the request.")
+	}
+}
+
 func main() {
+	// set default redirect for invalid labels
+	default_redirect = ""
+	if len(os.Args) > 1 {
+		u, err := url.Parse(os.Args[1])
+		if err == nil {
+			default_redirect = u.String()
+		}
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "This is your redirect server.")
+		RedirectDefault(w, r)
 	})
 
 	http.HandleFunc("GET /{label}", func(w http.ResponseWriter, r *http.Request) {
 		rv, err := redirect.GetRedirect(r.PathValue("label"))
 		if err != nil {
-			fmt.Fprintf(w, "error: %s", err)
-			return
+			RedirectDefault(w, r)
+		} else {
+			http.Redirect(w, r, rv.GetDestination(), http.StatusTemporaryRedirect)
 		}
-
-		http.Redirect(w, r, rv.GetDestination(), http.StatusTemporaryRedirect)
 	})
 
 	http.ListenAndServe(":8080", nil)
